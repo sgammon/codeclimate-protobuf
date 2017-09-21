83889a865d3c6b28d5d09d5548ec61a2f80de64a
Sam Gammon <sam@momentum.io>
2017-09-21 00:03:25 -0700

    Version bump fix -> 1.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='protolint',
-      version='1.1.3',
+      version='1.1.4',
       description='CodeClimate linter for Protobuf, using protoc-gen-lint',
       author='Sam Gammon',
       author_email='sam@bloombox.io',
